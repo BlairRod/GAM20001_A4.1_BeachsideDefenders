9633d4cf42223f5d60ac6f5ce743c18c6c120d8a
Blair Rod <103892145@student.swin.edu.au>
2024-05-05 11:01:46 +1000

    Scene switcher
    added scene switcher global to switch scenes with:
    SceneSwitcher.switch_scene(secene_path)

---
+++ Scenes/scene_switcher.gd
@@ -0,0 +1,22 @@
+extends Node
+
+# Helper for switching scenes
+# SceneSwitcher.switch_scene(secene_path)
+
+var current_scene = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var root = get_tree().root
+	current_scene = root.get_child(root.get_child_count() - 1)
+
+# Function called to switch scenes
+func scene_switcher(res_path) -> void:
+	call_deferred("_deffered_switch_scene", res_path)
+
+func _deffered_switch_scene(res_path) -> void:
+	current_scene.free()
+	var scene = load(res_path)
+	current_scene = scene.instantiate()
+	get_tree().root.add_child(current_scene)
+	get_tree().current_scene = current_scene
