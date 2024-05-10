b898135aa0d96bc4b16943ab2932bf6dca4c6630
Blair Rod <103892145@student.swin.edu.au>
2024-05-10 15:54:16 +1000

    Fixed player clamp
    Added padding and changed clamping in player dcript

--- Scripts/playercharacter.gd
+++ Scripts/playercharacter.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
 var speed = 200
+var padding = 20
 
 func _physics_process(delta):
 	var direction = Input.get_axis("left","right")
 	velocity = direction * speed * transform.x
 	move_and_slide()
 	
-	global_position = global_position.clamp(Vector2.ZERO, get_viewport_rect().size)
+	# Keep player on the screen
+	var viewRect := get_viewport_rect()
+	position.x = clamp(position.x, 0 + padding, viewRect.size.x - padding)
+	#global_position = global_position.clamp(Vector2.ZERO, get_viewport_rect().size)
