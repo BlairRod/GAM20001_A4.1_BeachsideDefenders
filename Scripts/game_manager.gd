70e7b1061ea495453a35e0080b08dd747672027d
Blair Rod <103892145@student.swin.edu.au>
2024-05-26 20:49:16 +1000

    Fixed pause button
    pause button (Esc) now opens and closes pause menu.

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -14,5 +14,5 @@ var game_paused : bool = false:
 
 func _input(event : InputEvent) -> void:
 	if(event.is_action_pressed("ui_cancel")):
-		$PauseCanvas.show()
+		print(game_paused)
 		game_paused = !game_paused
--- UI/pause_menu.gd
+++ UI/pause_menu.gd
@@ -7,11 +7,11 @@ func _ready() -> void:
 	hide()
 	game_manager.connect("toggle_game_paused", _on_game_manager_toggle_game_paused)
 	
-	# Key input event handling
-func _input(event : InputEvent):
-	# Esc key pauses game
-	if event.is_action_pressed("ui_cancel"):
-		game_manager.game_paused = false
+	## Key input event handling
+#func _input(event : InputEvent):
+	## Esc key pauses game
+	#if event.is_action_pressed("ui_cancel"):
+		#game_manager.game_paused = false
 
 func _on_game_manager_toggle_game_paused(is_paused : bool):
 	if(is_paused):
