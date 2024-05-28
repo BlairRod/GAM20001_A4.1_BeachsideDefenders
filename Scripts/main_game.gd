f840c31f301c736a20be0c7698461883914ddbdb
Jameselbangle <126848776+Jameselbangle@users.noreply.github.com>
2024-05-28 11:56:07 +1000

    Update main_game.gd

--- Scripts/main_game.gd
+++ Scripts/main_game.gd
@@ -1,21 +1,21 @@
 extends Node2D
 
-# Declare variables
+
 var player_lives = 3
 var score = 0
 onready var enemies = $EnemiesArray
 
 func _process(delta):
-    # Check if player lives are less than 0
+  
     if player_lives < 0:
         _game_over(false)
-    # Check if all enemies are defeated
+
     elif enemies.get_child_count() <= 0:
         score += 100 * player_lives
         _game_over(true)
 
 func _game_over(win):
-    # Get the Game Over scene
+
     var game_over_scene = load("res://GameOver.tscn").instance()
     game_over_scene.set("win", win)
     game_over_scene.set("score", score)
